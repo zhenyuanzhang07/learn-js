ac1bf1725a71170b6efadfe48c2c42a07e6e3917
Zhenyuan Zhang <zhang.zhenyuan@northeastern.edu>
2024-02-02 20:23:43 -0500

    created a JavaScript function logMsg()

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,22 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg(obj) {
+    if (obj && obj.hasOwnProperty('errMsg')) {
+        console.error(obj.errMsg);
+    } else {
+        console.log("The object does not contain an 'errMsg' property.");
+    }
+}
+
+// Object with errMsg
+const errorObject = {
+    errMsg: "An error occurred during processing."
+};
+
+// Object without errMsg
+const regularObject = {
+    message: "This is a regular message."
+};
+
+// Testing the function
+logMsg(errorObject); // Logs: "An error occurred during processing."
+logMsg(regularObject); // Logs: "The object does not contain an 'errMsg' property."
