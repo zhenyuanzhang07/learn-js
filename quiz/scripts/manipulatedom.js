fc7a5e0ea21a8bda414014f65bccce784af3adf1
Zhenyuan Zhang <zhang.zhenyuan@northeastern.edu>
2024-02-02 21:10:27 -0500

    edited manipulatedom.html and manipulatedom.js

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -6,7 +6,7 @@ function createTRNode(colNodes) {
   let trNode = document.createElement("tr");
   colNodes.forEach(function(colNode) {
     trNode.appendChild(colNode);
-  })
+  });
   return trNode;
 }
 
@@ -16,16 +16,35 @@ function createTDNode(childNode) {
   return tdNode;
 }
 
+function createEditLink() {
+  let editLink = document.createElement("a");
+  editLink.href = "#";
+  editLink.textContent = "Edit text";
+  editLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    let parentCell = event.target.parentNode;
+    let input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = "Enter Cell (x,y) ...";
+    parentCell.innerHTML = ""; // Clear the cell
+    parentCell.appendChild(input);
+    parentCell.appendChild(createEditLink()); // Add another edit link
+  });
+  return editLink;
+}
+
 function createTxtNode(txt) {
-  let txtNode = document.createTextNode(txt);
-  return txtNode;
+  return document.createTextNode(txt);
 }
 
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
-    let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let txtNode = createTxtNode("Cell (" + i + ", 0)");
+    let editLink = createEditLink();
+    let col1 = createTDNode(txtNode);
+    let col2 = createTDNode(editLink);
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
