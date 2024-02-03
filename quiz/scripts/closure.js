95c3bac24e7c2ecec5b1e736313699132dbd4a1e
Zhenyuan Zhang <zhang.zhenyuan@northeastern.edu>
2024-02-02 20:33:46 -0500

    updated closure.html and closure.js

--- quiz/scripts/closure.js
+++ quiz/scripts/closure.js
@@ -1,13 +1,13 @@
 function setColor(set) {
-        let changeColor = set;
-        if(changeColor) {
+    return function() {
+        if(set) {
             let userColor = document.getElementById('color').value;
             document.getElementById('myPara').style.color = userColor;
         }
-        
+    };
 }
 
 window.onload = function() {
-    let toggle = true;
+    let toggle = true; // Assuming 'toggle' might be used to enable/disable setColor functionality
     document.getElementById('btn').onclick = setColor(toggle);
 }
